5abf43c9ef0a88f7ca5b05f4e52dfa4a3b3c17ac
Linhxres <ruanvitor0941@gmail.com>
2025-01-26 09:38:44 -0300

    init

---
+++ dungeonaplication/src/main/java/dungeon/example/dungeonaplication/DungeonaplicationApplication.java
@@ -0,0 +1,13 @@
+package dungeon.example.dungeonaplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonaplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonaplicationApplication.class, args);
+	}
+
+}
---
+++ dungeonaplication/src/test/java/dungeon/example/dungeonaplication/DungeonaplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package dungeon.example.dungeonaplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonaplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
